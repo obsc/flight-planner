64cc6e3b873e21afaa01d8c2870fd783487f3ae1
saguziel <saguziel@gmail.com>
2013-12-02 11:46:02 -0500

    calculate distance

---
+++ util/distance.py
@@ -0,0 +1,26 @@
+from math import sin, cos, atan2
+
+class InvalidAirport(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
+'return distance between two points in km'
+def distance(lat1, lon1, lat2, lon2):
+  a = (sin(lat2 - lat1))**2.0 + cos(lat1) * cos(lat2) * (sin(lon2-lon1)/2.)**2.0
+  c = 2 * atan2(sqrt(a), sqrt(1-a))
+  return 6371 * c #6371 radius of earth in km
+
+def airportToLatLon(airportID):
+  f = open('dat/AirportsLatLong.csv')
+  f.readline()
+  for line in f:
+    stuff = line.split(',')
+    if stuff[0] == airportID:
+      lat = float(stuff[1])
+      lon = float(stuff[2])
+      return (lat, lon)
+    else:
+      pass
+  raise InvalidAirport(airportID + ' was not founds')
