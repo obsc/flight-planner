675e56a4f1498317390abe98524c5c4cf53cf0e1
saguziel <saguziel@gmail.com>
2013-12-03 01:05:03 -0500

    added nonworking distance function

--- util/distance.py
+++ util/distance.py
@@ -1,4 +1,5 @@
-from math import sin, cos, atan2, sqrt
+from math import atan, sin, cos, atan2, sqrt, asin, tan
+
 
 airportDict = dict()
 
@@ -24,6 +25,20 @@ def distance(lat1, lon1, lat2, lon2):
   a = (sin(lat2 - lat1))**2.0 + cos(lat1) * cos(lat2) * (sin(lon2-lon1)/2.)**2.0
   c = 2 * atan2(sqrt(a), sqrt(1-a))
   return 6371 * c #6371 radius of earth in km
+
+'''
+def distance2(lat1, lon1, lat2, lon2):
+  beta_1 = atan(tan(lat1))
+  beta_2 = atan(tan(lat2))
+  P = (beta_1 + beta_2)/2.0
+  Q = (beta_2 - beta_1)/2.0
+  centralAngle = 2 * asin(sqrt(sin((lat2-lat1)/2)**2. + cos(lat1) * cos(lat2) * (sin((lon2-lon1)/2))**2.))
+  X = (centralAngle - sin(centralAngle)) * (sin(P)*cos(Q))**2 / ((cos(centralAngle/2))**2.)
+  Y = (centralAngle + sin(centralAngle)) * (cos(P) * sin(Q))**2 / (sin(centralAngle/2))**2
+  a = 6371
+  distance = a * (centralAngle - (.5)*(X+Y))
+  return distance 
+'''
 
 def airportToLatLon(airportID):
   if airportID in airportDict:
