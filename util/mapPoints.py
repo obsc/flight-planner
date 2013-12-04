a609b44569a0a00f2ec52a8a8b1a39d7a677051b
saguziel <saguziel@gmail.com>
2013-12-04 00:25:40 -0500

    point by point classifier

---
+++ util/mapPoints.py
@@ -0,0 +1,55 @@
+from math import atan2, sin, cos
+
+def calcBearing(lat1, lon1, lat2, lon2):
+  lon_diff = lon2 - lon1
+  bearing = atan2( sin(lon_diff) * cos(lat2), cos(lat1) * sin(lat2) -sin(lat1)*cos(lat2)*cos(lon_diff))
+  return bearing
+
+def mapToVectors(asdipositionsorted, sortedFlightHistory, outfile):
+  f = open(asdipositionsorted)
+  g = open(outfile, 'w')
+  g.write('FlightHistoryID,Time,Latitude,Longitude,Angle,Bearing,NextLat,NextLon,Airport,Altitude,Speed\n')
+  airport_dict = mapToDestination(sortedFlightHistory)
+  line = f.readline()
+  stuff = line.strip().split(',')
+  curFHID = stuff[7]
+  lat = float(stuff[5])
+  lon = float(stuff[6])
+  time = stuff[1]
+  speed = int(stuff[4])
+  altitude = int(stuff[3])
+  for line in f:
+    stuff =line.strip().split(',')
+    try:
+      if stuff[7] == curFHID:
+        bearing = calcBearing(lat, lon, float(stuff[5]), float(stuff[6]))
+        angle = atan2(float(stuff[5]) - lat, float(stuff[6]) - lon)
+        g.write('%s,%s,%f,%f,%f,%f,%f,%f,%s,%i,%i\n' % (curFHID, time, lat, lon, angle,bearing,float(stuff[5]),float(stuff[6]),airport_dict[curFHID], altitude, speed))
+      else:
+        pass
+      curFHID = stuff[7]
+      lat = float(stuff[5])
+      lon = float(stuff[6])
+      time = stuff[1]
+      speed = int(stuff[4])
+      altitude = int(stuff[3])
+    except ValueError:
+      print 'GODDAMMIT'
+  f.close()
+  g.close()
+
+def mapToDestination(sortedFlightHistory):
+  f = open(sortedFlightHistory)
+  f.readline()
+  fhid_dict = dict()
+  for line in f:
+    try:
+      stuff = line.strip().split(',')
+      fhid = stuff[0]
+      airport = stuff[7]
+      fhid_dict[fhid] = airport
+    except ValueError:
+      print 'FUUUUUCK'
+  f.close()
+  print 'done'
+  return fhid_dict
